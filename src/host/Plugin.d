b4f2121544c50be7e8c71eb8c6bb613ade8f2c95
gnuvebox <gnuvebox@gnuvebox-VirtualBox.(none)>
2013-07-25 17:54:15 +0100

    adding mongoose cpp wrapper

--- src/host/Plugin.d
+++ src/host/Plugin.d
@@ -1,20 +1,11 @@
-src/host/plugins/FuzzTest.o: src/host/plugins/FuzzTest.cpp src/host/plugins/FuzzTest.h \
- src/host/plugins/global.h src/host/plugins/../../settings.h \
- libs/portaudio/include/portaudio.h /usr/include/math.h \
- /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h \
+src/host/Plugin.o: src/host/Plugin.cpp src/host/Plugin.h \
+ libs/portaudio/include/portaudio.h src/cJSON.h src/uthash.h \
+ /usr/include/string.h /usr/include/features.h \
+ /usr/include/i386-linux-gnu/bits/predefs.h \
  /usr/include/i386-linux-gnu/sys/cdefs.h \
  /usr/include/i386-linux-gnu/bits/wordsize.h \
  /usr/include/i386-linux-gnu/gnu/stubs.h \
  /usr/include/i386-linux-gnu/gnu/stubs-32.h \
- /usr/include/i386-linux-gnu/bits/huge_val.h \
- /usr/include/i386-linux-gnu/bits/huge_valf.h \
- /usr/include/i386-linux-gnu/bits/huge_vall.h \
- /usr/include/i386-linux-gnu/bits/inf.h \
- /usr/include/i386-linux-gnu/bits/nan.h \
- /usr/include/i386-linux-gnu/bits/mathdef.h \
- /usr/include/i386-linux-gnu/bits/mathcalls.h \
- src/host/plugins/delayline.h src/host/plugins/../Plugin.h src/cJSON.h \
- src/uthash.h /usr/include/string.h \
  /usr/lib/gcc/i686-linux-gnu/4.7/include/stddef.h /usr/include/xlocale.h \
  /usr/include/stdlib.h /usr/include/i386-linux-gnu/bits/waitflags.h \
  /usr/include/i386-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
