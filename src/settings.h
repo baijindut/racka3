8cf16d75f1206b3172c58d3c0b3745a248eb7f97
mrchay <chay@mrchay.com>
2013-09-19 16:32:56 +0100

    removed juce
    got annoyed with it

--- src/settings.h
+++ src/settings.h
@@ -12,14 +12,16 @@
 
 static int SAMPLE_RATE	= (44100);
 static float fSAMPLE_RATE = (44100.0);
-static float cSAMPLE_RATE = 1 / (44100.0);
+static float cSAMPLE_RATE = (float)(1 / (44100.0))
 
 #define PA_SAMPLE_TYPE      paFloat32
-extern int FRAMES_PER_BUFFER;  					// <--- this is extremely shit and needs refactoring.
 #define PERIOD				 FRAMES_PER_BUFFER
 #define fPERIOD				 ((float)FRAMES_PER_BUFFER)
-typedef float SAMPLE;
+#define SAMPLE				float
 #define INTERLEAVED			paNonInterleaved
+
+// this is extremely shit and needs refactoring:
+extern int FRAMES_PER_BUFFER;
 
 #define STORAGE_DIR			"www/data/"
 
